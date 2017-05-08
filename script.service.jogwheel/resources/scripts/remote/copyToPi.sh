a30fc2b5470ca7cbc7d2111c4bdf22b510cb4988
spielzeugland <spielzeugland@users.noreply.github.com>
2017-05-08 22:03:43 +0200

    Fix target location in copy script

--- script.service.jogwheel/resources/scripts/remote/copyToPi.sh
+++ script.service.jogwheel/resources/scripts/remote/copyToPi.sh
@@ -2,5 +2,5 @@ host=osmc
 user=osmc
 pluginName="script.service.jogwheel"
 pluginFolder="~/.kodi/addons"
-scp -r -p ./../../../../$pluginName $user@$host:$pluginFolder/$pluginName/
-ssh $user@$host $pluginFolder/$pluginName/resources/scripts/local/install_dependencies.sh
+scp -r -p ./../../../../$pluginName $user@$host:$pluginFolder/
+# ssh $user@$host $pluginFolder/$pluginName/resources/scripts/local/install_dependencies.sh
